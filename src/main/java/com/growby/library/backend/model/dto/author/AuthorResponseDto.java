b1ff5f1f7cdfe0e4f34117e9346daecc95334cb5
Paúl Guevara <pauljgl2008@gmail.com>
2024-11-23 11:38:47 -0500

    refactor: clean code

--- src/main/java/com/growby/library/backend/model/dto/author/AuthorResponseDto.java
+++ src/main/java/com/growby/library/backend/model/dto/author/AuthorResponseDto.java
@@ -5,8 +5,6 @@ import lombok.Data;
 
 import java.time.LocalDate;
 
-import static com.growby.library.backend.common.ValidationConstants.PUBLICATION_DATE_PARAM;
-
 @Data
 public class AuthorResponseDto {
 
